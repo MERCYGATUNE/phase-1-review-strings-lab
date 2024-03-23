729a3ae4ea71d8b1259fc9449169960e17868777
MERCYGATUNE <ariri691@gmail.com>
2024-03-23 14:46:44 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, Welcome to Flatbook ,Grace Hopper!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, WELCOME TO FLATBOOK,GRACE HOPPER!';
+const upperCaseCurrentUser = currentUser.toUpperCase;
+
+const shortGreeting = "Welcome, Welcome,GRACE!";
+const firstInitial = currentUser[0];
